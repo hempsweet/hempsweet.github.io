9f2b9ee2c410583623ad9a3066993671c651279b
MrAnderson <github@thematrix.email>
2017-10-03 15:48:27 -0600

    received from devs

---
+++ header.php
@@ -0,0 +1,86 @@
+<head>
+
+    <!-- Basic Page Needs -->
+    <meta charset="utf-8">
+    <title>Be</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Mobile Specific Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+
+    <!-- FONTS -->
+    <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Roboto:100,300,400,400italic,700'>
+    <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Patua+One:100,300,400,400italic,700'>
+    <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Lato:100,300,400,400italic,700,700italic'>
+    <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Caudex:100,300,400,400italic,700,700italic'>
+
+    <!-- CSS -->
+    <link rel='stylesheet' href='css/global.css'>
+    <link rel='stylesheet' href='cssherbal/structure.css'>
+    <link rel='stylesheet' href='cssherbal/herbal.css'>
+    <link rel='stylesheet' href='cssherbal/custom.css'>
+	<link rel="stylesheet" href="cssherbal/jquery-ui.css">
+  
+
+</head>
+
+<body class="archive post-type-archive post-type-archive-product woocommerce woocommerce-page color-custom style-simple layout-full-width nice-scroll-on button-flat if-overlay if-border-hide header-plain header-fw minimalist-header-no sticky-header sticky-tb-color ab-hide subheader-both-center menuo-no-borders menuo-right mobile-tb-left mobile-mini-mr-ll mobile-header-mini">
+    <div id="Wrapper">
+        <div id="Header_wrapper">
+            <header id="Header">
+                <div class="header_placeholder"></div>
+                <div id="Top_bar">
+                    <div class="container">
+                        <div class="column one">
+                            <div class="top_bar_left clearfix">
+                                <div class="logo">
+                                    <a id="logo" href="../../index-herbal.html" title="BeHerbal - BeTheme"><img class="logo-main scale-with-grid" src="images/herbal.png" alt="herbal" />
+                                    </a>
+                                </div>
+                                <div class="menu_wrapper">
+                                    <nav id="menu" class="menu-main-menu-container">
+                                        <ul id="menu-main-menu" class="menu">
+                                            <li>
+                                                <a href="../../index-herbal.html"><span>Home</span></a>
+                                            </li>
+                                            <li class="current_page_item">
+                                                <a href="shop.html"><span>Shop</span></a>
+                                            </li>
+                                            <li>
+                                                <a href="about.html"><span>About</span></a>
+                                            </li>
+                                            <li>
+                                                <a href="faq.html"><span>FAQ</span></a>
+                                            </li>
+                                            <li>
+                                                <a href="contact.html"><span>Contact</span></a>
+                                            </li>
+                                            <li>
+                                                <a target="_blank" href="http://bit.ly/1M6lijQ"><span><span style="padding: 0; color: #d8b873;">Buy now</span></span></a>
+                                            </li>
+                                        </ul>
+                                    </nav><a class="responsive-menu-toggle" href="#"><i class="icon-menu-fine"></i></a>
+                                </div>
+                                <div class="secondary_menu_wrapper"></div>
+                                <div class="banner_wrapper"></div>
+                                <div class="search_wrapper">
+                                    <form method="get" id="searchform" action="#">
+                                        <i class="icon_search icon-search-fine"></i><a href="#" class="icon_close"><i class="icon-cancel-fine"></i></a>
+                                        <input type="text" class="field" name="s" id="s" placeholder="Enter your search" />
+                                        <input type="submit" class="submit" value="" style="display:none;" />
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="top_bar_right">
+                                <div class="top_bar_right_wrapper">
+                                    <a id="header_cart" href="#"><i class="icon-bag-fine"></i><span>0</span></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
